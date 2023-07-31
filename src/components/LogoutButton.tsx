6adc7540232a66ff5d5d6ad44ca52288548c6104
Shirahamah <hiropi1224s@gmail.com>
2023-07-31 22:35:23 +0900

    🎉

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+
+export default function LogoutButton() {
+  const router = useRouter();
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createClientComponentClient();
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <button
+      className='rounded-md bg-btn-background px-4 py-2 no-underline hover:bg-btn-background-hover'
+      onClick={signOut}
+    >
+      Logout
+    </button>
+  );
+}
