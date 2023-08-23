2bde4c57693db996f1a83a54ebaa80ba7e11d2c3
Shirahamah <hiropi1224s@gmail.com>
2023-08-23 23:28:21 +0900

    #13 mypage追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,6 +59,12 @@ export default async function Index() {
             {user ? (
               <div className='flex items-center gap-4'>
                 Hey, {user.email}!
+                <Link
+                  href='/mypage'
+                  className='rounded-md bg-btn-background px-4 py-2 no-underline hover:bg-btn-background-hover'
+                >
+                  マイページ
+                </Link>
                 <LogoutButton />
               </div>
             ) : (
